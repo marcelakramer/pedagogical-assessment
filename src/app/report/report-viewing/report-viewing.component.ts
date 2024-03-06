e64290f78e3d262d47cd5e0f5a52c7562d420ea1
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-03-05 22:56:20 -0300

    implement calcSpecificsAverages method

--- src/app/report/report-viewing/report-viewing.component.ts
+++ src/app/report/report-viewing/report-viewing.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Teacher } from '../../shared/models/teacher';
 import { AssessmentRating } from '../../shared/interfaces/assessment-rating';
 import { assessments } from '../../shared/assessments';
-// import { SpecificsAverages } from '../../shared/interfaces/specifics-averages';
+import { SpecificsAverages } from '../../shared/interfaces/specifics-averages';
 
 @Component({
   selector: 'app-report-viewing',
@@ -12,16 +12,16 @@ import { assessments } from '../../shared/assessments';
 export class ReportViewingComponent {
   teacher: Teacher = new Teacher('1', '130089', 'Alecsandro Monteiro Kramer', assessments)
   overallAverage: number = this.calcOverallAverage(this.teacher.assessments)
-  // specificsAverages: Array<SpecificsAverages> = this.calcSpecificsAverages(this.teacher.assessments);
+  specificsAverages: Array<SpecificsAverages> = this.calcSpecificsAverages(this.teacher.assessments);
 
   calcOverallAverage(assessments: Array<AssessmentRating>): number {
     let ratingTotal = 0;
     let criterionsQuantity = 0;
 
     assessments.forEach(item => {
-      Object.values(item).forEach(categoria => {
-        Object.values(categoria).forEach(rate => {
-            ratingTotal += rate;
+      Object.values(item).forEach(aspect => {
+        Object.values(aspect).forEach(criterionRate => {
+            ratingTotal += criterionRate;
             criterionsQuantity++;
           });
         });
@@ -31,5 +31,33 @@ export class ReportViewingComponent {
     return overallAverage;
   }
 
-  // calcSpecificsAverages(assessments: Array<AssessmentRating>): Array<SpecificsAverages> {}
+  calcSpecificsAverages(assessments: Array<AssessmentRating>): Array<SpecificsAverages> {
+    const averages: SpecificsAverages[] = [];
+
+    for (const aspect in assessments[0]) {
+        const aspectAverages: number[] = [];
+        const criteriaAverages: { criterion: string, average: number }[] = [];
+
+        for (const criterion in assessments[0][aspect]) {
+            let total = 0;
+
+  
+            assessments.forEach(assessment => {
+                total += assessment[aspect][criterion];
+            });
+
+            criteriaAverages.push({ criterion, average: total / assessments.length });
+            aspectAverages.push(total / assessments.length);
+        }
+
+        const aspectAverage = aspectAverages.reduce((acc, curr) => acc + curr, 0) / aspectAverages.length;
+
+        averages.push({
+            aspect,
+            average: aspectAverage,
+            criteriaAverage: criteriaAverages
+        });
+    }
+    return averages;
+  }
 }
