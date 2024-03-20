92a09b323e8a033b8489df5a062928d044f28eb8
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-03-19 22:52:25 -0300

    create subject model

--- src/app/shared/models/assessment.ts
+++ src/app/shared/models/assessment.ts
@@ -1,9 +1,12 @@
+import { Subject } from "./subject";
 import { Teacher } from "./teacher";
 
 export class Assessment {
     constructor(
         public id: string,
         public teacher: Teacher,
+        public subject: Subject,
+        public year: number,
         public rating: {}
     ) {};
 };
