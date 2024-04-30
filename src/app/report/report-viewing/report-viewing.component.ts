5dad6169b72058bb6fb58b4e461cf03d0c07a4e7
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-04-29 21:05:14 -0300

    no assessments bug fix

--- src/app/report/report-viewing/report-viewing.component.ts
+++ src/app/report/report-viewing/report-viewing.component.ts
@@ -146,37 +146,44 @@ export class ReportViewingComponent implements OnInit {
   calcSpecificsAverages(assessments: Array<Assessment>): Array<SpecificsAverages> {
     const averages: SpecificsAverages[] = [];
     
-    for (const dimension in assessments[0].rating) {
-      let dimensionColor = ''
-      criteria.some(item => {
-        if (dimension == item.dimension) {
-          dimensionColor = item.color;
+    if (this.hasAssessments()) {
+      for (const dimension in assessments[0].rating) {
+        let dimensionColor = ''
+        criteria.some(item => {
+          if (dimension == item.dimension) {
+            dimensionColor = item.color;
+          }
+        })
+        const dimensionAverages: number[] = [];
+        const sentenceAverages: { sentence: string, average: number }[] = [];
+        
+        for (const sentence in assessments[0].rating[dimension]) {
+          let total = 0;
+          
+          assessments.forEach(assessment => {
+            total += assessment.rating[dimension][sentence];
+          });
+          
+          sentenceAverages.push({ sentence: sentence, average: (total / assessments.length) * 2 });
+          dimensionAverages.push(total / assessments.length);
         }
-      })
-      const dimensionAverages: number[] = [];
-      const sentenceAverages: { sentence: string, average: number }[] = [];
-      
-      for (const sentence in assessments[0].rating[dimension]) {
-        let total = 0;
         
-        assessments.forEach(assessment => {
-          total += assessment.rating[dimension][sentence];
+        const aspectAverage = dimensionAverages.reduce((acc, curr) => acc + curr, 0) / dimensionAverages.length * 2;
+        
+        averages.push({
+          dimension,
+          dimensionColor: dimensionColor,
+          average: aspectAverage,
+          sentenceAverage: sentenceAverages
         });
-        
-        sentenceAverages.push({ sentence: sentence, average: (total / assessments.length) * 2 });
-        dimensionAverages.push(total / assessments.length);
       }
-      
-      const aspectAverage = dimensionAverages.reduce((acc, curr) => acc + curr, 0) / dimensionAverages.length * 2;
-      
-      averages.push({
-        dimension,
-        dimensionColor: dimensionColor,
-        average: aspectAverage,
-        sentenceAverage: sentenceAverages
-      });
     }
     return averages;
+  }
+
+  hasAssessments = () => {
+    console.log(this.assessments.length > 0)
+    return this.assessments.length > 0;
   }
 
   selectAverageOption = (eventTarget: EventTarget | null): void => {
@@ -195,7 +202,6 @@ export class ReportViewingComponent implements OnInit {
   };
 
   hasWarningDimension = (): boolean => {
-    console.log(this.specificsAverages)
     return this.specificsAverages.some(dimension => dimension.average < 6);
   }
 
